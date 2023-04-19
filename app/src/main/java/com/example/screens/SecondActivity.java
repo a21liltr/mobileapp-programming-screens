e60c5559f34a201543c47216f32cfdad4af7ed14
a21liltr <a21liltr@student.his.se>
2023-04-19 13:00:24 +0200

    Added finish(); to close SecondActivity when clicking closeButton.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +11,20 @@ import com.example.screens.databinding.ActivityTwoBinding;
 
 public class SecondActivity extends AppCompatActivity {
     private ActivityTwoBinding twoBinding;
+    private Button closeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         twoBinding = ActivityTwoBinding.inflate(getLayoutInflater());
         setContentView(twoBinding.getRoot());
+
+        closeButton = twoBinding.activityTwoGoBack;
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
