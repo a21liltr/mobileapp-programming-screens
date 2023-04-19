d383fdbff15b1150187bf4c530ce4c366f8e4eef
a21liltr <a21liltr@student.his.se>
2023-04-19 12:10:03 +0200

    Added viewBinding.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.screens.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private Button goToActivityTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToActivityTwo = binding.activityMainGoToActivityTwo;
+
     }
 }
