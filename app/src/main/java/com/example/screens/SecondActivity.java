d5f55771245c63119a22460f6222f3ce26867d81
a21liltr <a21liltr>
2023-04-20 10:57:22 +0200

    Added a random value for fun.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -7,26 +7,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.screens.databinding.ActivityMainBinding;
 import com.example.screens.databinding.ActivityTwoBinding;
 
+import java.util.Random;
+
 public class SecondActivity extends AppCompatActivity {
-    private ActivityTwoBinding twoBinding;
-    private Button closeButton;
-    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        twoBinding = ActivityTwoBinding.inflate(getLayoutInflater());
+        com.example.screens.databinding.ActivityTwoBinding twoBinding = ActivityTwoBinding.inflate(getLayoutInflater());
         setContentView(twoBinding.getRoot());
 
-        closeButton = twoBinding.activityTwoGoBack;
-        output = twoBinding.activityTwoOutput;
+        Button closeButton = twoBinding.activityTwoGoBack;
+        TextView output = twoBinding.activityTwoOutput;
+
+        Random rand = new Random();
+        int upperbound = 250;
+        int years = rand.nextInt(upperbound);
 
         String input = getIntent().getStringExtra("keyInput");
 
-        output.setText(input);
+        output.setText(input.toUpperCase() + ", they were " + years + " old!");
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
